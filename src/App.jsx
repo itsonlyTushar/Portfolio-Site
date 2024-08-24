f19ec68eca5d1de8aa834590af8a127602cd2ce9
Tushar Soni <tushargsoni@outlook.com>
2024-08-24 12:39:42 +0530

    Analytics update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from './component/Hero'
 import './App.css';
-
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
   return (
     <div>
+      <Analytics/>
       <Hero />
     </div>
   )
